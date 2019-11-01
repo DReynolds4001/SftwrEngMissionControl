d743f8c501d7dfaac751c3416933eca0fee7970d
Daniel <Daniel@TKE3D.Com>
2019-11-01 16:24:39 -0400

    Original file placed in "Recent" list after performing a Save As

--- PowerEditor/src/NppIO.cpp
+++ PowerEditor/src/NppIO.cpp
@@ -1513,15 +1513,20 @@ bool Notepad_plus::fileSaveAs(BufferID id, bool isSaveCopy)
 
 	if (pfn)
 	{
+
 		BufferID other = _pDocTab->findBufferByName(pfn);
 		if (other == BUFFER_INVALID)
 			other = _pNonDocTab->findBufferByName(pfn);
 
 		if (other == BUFFER_INVALID)	//can save, as both (same and other) view don't contain buffer
 		{
+			if (!buf->isUntitled())
+				_lastRecentFileList.add(bufferID->getFullPathName());
+
 			bool res = doSave(bufferID, pfn, isSaveCopy);
 			//buf->setNeedsLexing(true);	//commented to fix wrapping being removed after save as (due to SCI_CLEARSTYLE or something, seems to be Scintilla bug)
 			//Changing lexer after save seems to work properly
+
 			return res;
 		}
 		else		//cannot save, other view has buffer already open, activate it
